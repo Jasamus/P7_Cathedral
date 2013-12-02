a78cde16fe6d3a06037a893107ec4616795918ae
Jasamus <jasamus@gmail.com>
2013-12-02 05:29:43 -0600

    Basic pass of a single direction stretch without doodad spawn or scaled translation

---
+++ Assets/Scripts/StretchBase.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * This class manages all of the stretching of objects
+ * 
+ * TODO: change the UV scaling to include
+ */
+public class StretchBase : MonoBehaviour {
+	
+	public Vector3 stretchDir = new Vector3(0.0f,1.0f,0.0f);
+	public Vector2 uvStretchDir = new Vector2(0.0f, 1.0f);
+	public float stretchDist = 20.0f;
+	
+	public float maxStretchSpeed = 0.8f;
+
+	private Transform staticStretch;
+	private Transform xStretch;
+	private Transform yStretch;
+	private Transform zStretch;
+
+	//public float prevStretchPercent = 0.0f;
+	private float currentStretchPercent;
+	private float targetStretchPercent;
+	private bool bForceUpdate;
+
+	// Use this for initialization
+	void Start () {
+		staticStretch = transform.Find("NoScale");
+		xStretch = transform.Find("XScale");
+		yStretch = transform.Find("YScale");
+		zStretch = transform.Find("ZScale");
+
+		if(staticStretch.localPosition != Vector3.zero)
+			Debug.LogWarning("No Scale failed to start at position 0,0,0");
+		if(xStretch.localPosition != Vector3.zero)
+			Debug.LogWarning("X Scale failed to start at position 0,0,0");
+		if(yStretch.localPosition != Vector3.zero)
+			Debug.LogWarning("Y Scale failed to start at position 0,0,0");
+		if(zStretch.localPosition != Vector3.zero)
+			Debug.LogWarning("Z Scale failed to start at position 0,0,0");
+
+		//UpdateStretch(1.0f);
+	}
+
+	public void UpdateStretch(float newStretchPercent, bool bImmediateUpdate = false){
+		targetStretchPercent = newStretchPercent;
+		if(bImmediateUpdate){
+			//currentStretchPercent = newStretchPercent;
+			bForceUpdate = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!bForceUpdate && targetStretchPercent == currentStretchPercent)
+			return;
+
+		float prevStretchPercent = currentStretchPercent;
+		float stretchChange = maxStretchSpeed * Time.deltaTime;
+
+		if(bForceUpdate){
+			bForceUpdate = false;
+			currentStretchPercent = targetStretchPercent;
+		}else{
+			if(Mathf.Abs(targetStretchPercent - currentStretchPercent) > stretchChange){
+				if(targetStretchPercent < currentStretchPercent)
+					currentStretchPercent -= stretchChange;
+				else
+					currentStretchPercent += stretchChange;	
+			}else
+				currentStretchPercent = targetStretchPercent;
+		}
+
+		staticStretch.transform.localPosition = stretchDir * stretchDist * currentStretchPercent;
+		
+		if(xStretch != null){
+			xStretch.transform.localScale = new Vector3((stretchDir.x * stretchDist * currentStretchPercent) + 1.0f,1.0f,1.0f);
+			
+			if(stretchDir.x != 0.0f){
+				Renderer[] cr = xStretch.GetComponentsInChildren<Renderer>(); 
+				
+				foreach(Renderer renderer in cr){
+					for(int i = 0; i < renderer.materials.Length; i++){
+						Vector2 prevMatScale = renderer.materials[i].GetTextureScale("_MainTex");
+						
+						if(uvStretchDir.x != 0.0f){
+							prevMatScale.x = (prevMatScale.x / ((stretchDist * prevStretchPercent) + 1.0f)) * ((stretchDist * currentStretchPercent) + 1.0f);
+						}
+						if(uvStretchDir.y != 0.0f){
+							prevMatScale.y = (prevMatScale.y / ((stretchDist * prevStretchPercent) + 1.0f)) * ((stretchDist * currentStretchPercent) + 1.0f);
+						}
+						renderer.materials[i].SetTextureScale("_MainTex", prevMatScale);
+					}
+				}
+			}
+		}
+		
+		if(yStretch != null){
+			yStretch.transform.localScale = new Vector3(1.0f,(stretchDir.y * stretchDist * currentStretchPercent) + 1.0f,1.0f);
+			
+			if(stretchDir.y != 0.0f){
+				Renderer[] cr = yStretch.GetComponentsInChildren<Renderer>(); 
+				
+				foreach(Renderer renderer in cr){
+					for(int i = 0; i < renderer.materials.Length; i++){
+						Vector2 prevMatScale = renderer.materials[i].GetTextureScale("_MainTex");
+						
+						if(uvStretchDir.x != 0.0f){
+							prevMatScale.x = (prevMatScale.x / ((stretchDist * prevStretchPercent) + 1.0f)) * ((stretchDist * currentStretchPercent) + 1.0f);
+						}
+						if(uvStretchDir.y != 0.0f){
+							prevMatScale.y = (prevMatScale.y / ((stretchDist * prevStretchPercent) + 1.0f)) * ((stretchDist * currentStretchPercent) + 1.0f);
+						}
+						renderer.materials[i].SetTextureScale("_MainTex", prevMatScale);
+					}
+				}
+			}
+		}
+		
+		if(zStretch != null){
+			zStretch.transform.localScale = new Vector3(1.0f,1.0f,(stretchDir.z * stretchDist * currentStretchPercent) + 1.0f);
+			
+			if(stretchDir.z != 0.0f){
+				Renderer[] cr = zStretch.GetComponentsInChildren<Renderer>(); 
+				
+				foreach(Renderer renderer in cr){
+					for(int i = 0; i < renderer.materials.Length; i++){
+						Vector2 prevMatScale = renderer.materials[i].GetTextureScale("_MainTex");
+						
+						if(uvStretchDir.x != 0.0f){
+							prevMatScale.x = (prevMatScale.x / ((stretchDist * prevStretchPercent) + 1.0f)) * ((stretchDist * currentStretchPercent) + 1.0f);
+						}
+						if(uvStretchDir.y != 0.0f){
+							prevMatScale.y = (prevMatScale.y / ((stretchDist * prevStretchPercent) + 1.0f)) * ((stretchDist * currentStretchPercent) + 1.0f);
+						}
+						renderer.materials[i].SetTextureScale("_MainTex", prevMatScale);
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/StretchTriggerLookDir.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StretchTriggerLookDir : MonoBehaviour {
+	
+	private bool bUpdate;
+	private Transform playerCam;
+
+	public GameObject[] connectedStretch;
+	private StretchBase[] stretch;
+	public Vector3 stretchDir = new Vector3(0.0f,1.0f,0.01f);
+	public float maxStretchAngle = 30.0f; //The angle away from looking in the correct direction that the movement will have maxed out
+	public float startStretchAngle = 80.0f; //The angle away from looking in the correct direction that the movement will start
+	
+	// Use this for initialization
+	void Start () {
+		stretch = new StretchBase[connectedStretch.Length];
+
+		for(int i = 0; i < connectedStretch.Length; i++){
+			stretch[i] = connectedStretch[i].GetComponent<StretchBase>();
+		}
+
+		if(stretch.Length == 0){
+			Debug.LogWarning("No connected stretcher to this...disabling");
+			enabled = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player"){
+			bUpdate = true;
+			playerCam = other.GetComponentInChildren<Camera>().transform;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.tag == "Player"){
+			bUpdate = false;
+			for(int i = 0; i < stretch.Length; i++){
+				stretch[i].UpdateStretch(0.0f,true);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(bUpdate){
+			//make sure we are at least looking at the direction before we care at all
+			float viewAngle = Vector3.Angle(playerCam.forward, stretchDir);
+			if(viewAngle < startStretchAngle){
+				if(viewAngle < maxStretchAngle){
+					viewAngle = 1.0f;
+				}else{
+					viewAngle = 1.0f - ((viewAngle - maxStretchAngle) / (startStretchAngle -maxStretchAngle));
+				}
+			}else
+				viewAngle = 0.0f;
+
+			for(int i = 0; i < stretch.Length; i++){
+				if(stretch[i] != null)
+					stretch[i].UpdateStretch(viewAngle);
+			}
+		}
+	}
+}

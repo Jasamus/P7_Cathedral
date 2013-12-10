235edafb6d0f7c401779356d13044404a2e97c13
Mary Scoville <scovillemc@gmail.com>
2013-12-10 16:11:41 -0600

    All files are together now

---
+++ Assets/Stuff/Scripts/PostProcessManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostProcessManager : MonoBehaviour {
+	
+	private Bloom bloom;
+	private DepthOfFieldScatter dof;
+	
+	private bool bUpdateBloom;
+	private bool bUpdateDoF;
+	
+	public float targetBloomStrength;
+	public float targetDofAperture;
+	
+	public float transitionSpeed = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+		bloom = GetComponent<Bloom>();
+		if(bloom != null){
+			bUpdateBloom = true;
+			targetBloomStrength = bloom.bloomIntensity;
+		}
+		dof = GetComponent<DepthOfFieldScatter>();
+		if(dof != null){
+			bUpdateDoF = true;
+			targetDofAperture = dof.aperture;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(bUpdateBloom){
+			bloom.bloomIntensity = Mathf.Lerp(bloom.bloomIntensity, targetBloomStrength, transitionSpeed * Time.deltaTime);
+		}
+		
+		if(bUpdateDoF){
+			dof.aperture = Mathf.Lerp(dof.aperture, targetDofAperture, transitionSpeed * Time.deltaTime);
+		}
+	}
+}
+
---
+++ Assets/Stuff/Scripts/PostProcessVolume.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostProcessVolume : MonoBehaviour {
+
+	public float bloomIntensity;
+	public float dofAperture;
+	
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Player")){
+			PostProcessManager ppm = other.GetComponentInChildren<PostProcessManager>();
+			ppm.targetBloomStrength = bloomIntensity;
+			ppm.targetDofAperture = dofAperture;
+		}
+	}
+}
